b533e946f4c8c29954c28a981fa4390fff679188
tstanley090 <tyestanley090@gmail.com>
2025-04-29 13:12:05 -0700

    removed server to its own repo and updated client file structure

--- server/MusicPlayer.Core/Interfaces/IFileStorageService.cs
+++
@@ -1,8 +0,0 @@
-namespace MusicPlayer.Core.Interfaces;
-
-public interface IFileStorageService
-{
-  Task<Stream> GetFileAsync(string filePath);
-  Task<string> GetSasTokenAsync(string filePath, TimeSpan expiryTime);
-  Task<string> GetBlobUrlAsync(string filePath);
-}
--- server/MusicPlayer.Core/Interfaces/IMusicService.cs
+++
@@ -1,9 +0,0 @@
-using MusicPlayer.Core.Models;
-
-namespace MusicPlayer.Core.Interfaces;
-
-public interface IMusicService
-{
-  Task<IEnumerable<Song>> GetAllSongsAsync();
-  Task<Song?> GetSongByIdAsync(Guid id);
-}
