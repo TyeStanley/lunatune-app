61dbcbf32dc0c65bd33be424fee5a517251a98b6
tstanley090 <tyestanley090@gmail.com>
2025-05-11 23:43:46 -0700

    removed search bar on nav

--- src/components/nav/AppNav.tsx
+++ src/components/nav/AppNav.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { Search } from 'lucide-react';
-import { useRouter } from 'next/navigation';
 import AuthButton from '../auth/AuthButton';
 import NavLogo from './NavLogo';
 import { useTauriCheck } from '@/hooks/useTauriCheck';
@@ -9,31 +7,13 @@ import InstallAppButton from './InstallAppButton';
 
 export default function AppNav() {
   const isDesktop = useTauriCheck();
-  const router = useRouter();
-
-  const handleSearchFocus = () => {
-    router.push('/search');
-  };
 
   return (
     <nav className="border-background-lighter relative border-b">
       <div className="from-background-light/80 via-background/60 to-primary/10 absolute inset-0 z-0 bg-gradient-to-r" />
       <div className="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between gap-8">
-          <div className="flex items-center gap-8">
-            <NavLogo />
-
-            {/* Search Bar */}
-            <div className="bg-background-lighter hidden items-center gap-3 rounded-lg px-4 py-2 sm:flex">
-              <Search className="text-gray-400" size={20} />
-              <input
-                type="text"
-                placeholder="Search songs, artists, albums"
-                onFocus={handleSearchFocus}
-                className="bg-background-lighter w-64 text-gray-200 placeholder-gray-400 focus:outline-none"
-              />
-            </div>
-          </div>
+          <NavLogo />
 
           <div className="flex items-center gap-4">
             {!isDesktop && <InstallAppButton />}
