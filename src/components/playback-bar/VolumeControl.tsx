036df7b7a9b0152e1a502df5e754cb650133f0df
tstanley090 <tyestanley090@gmail.com>
2025-06-10 15:17:42 -0700

    updated volume control to remember current sound volume

--- src/components/playback-bar/VolumeControl.tsx
+++ src/components/playback-bar/VolumeControl.tsx
@@ -4,24 +4,39 @@ import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { setVolume } from '@/redux/state/playback-controls/playbackControlsSlice';
 import { Volume1, Volume2, VolumeX } from 'lucide-react';
 import Slider from '../ui/Slider';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
+
+const VOLUME_STORAGE_KEY = 'lunatune-volume';
 
 export default function VolumeControl() {
   const dispatch = useAppDispatch();
   const { volume } = useAppSelector((state) => state.playbackControls);
   const [previousVolume, setPreviousVolume] = useState(1);
 
+  // Initialize volume from local storage on component mount
+  useEffect(() => {
+    const storedVolume = localStorage.getItem(VOLUME_STORAGE_KEY);
+    if (storedVolume !== null) {
+      const parsedVolume = parseFloat(storedVolume);
+      dispatch(setVolume(parsedVolume));
+      setPreviousVolume(parsedVolume);
+    }
+  }, [dispatch]);
+
   const handleVolumeClick = useCallback(() => {
     if (volume > 0) {
       setPreviousVolume(volume);
       dispatch(setVolume(0));
+      localStorage.setItem(VOLUME_STORAGE_KEY, '0');
     } else {
       dispatch(setVolume(previousVolume));
+      localStorage.setItem(VOLUME_STORAGE_KEY, previousVolume.toString());
     }
   }, [volume, previousVolume, dispatch]);
 
   const handleVolumeChange = (value: number) => {
     dispatch(setVolume(value));
+    localStorage.setItem(VOLUME_STORAGE_KEY, value.toString());
   };
 
   const getVolumeIcon = () => {
